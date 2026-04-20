b583206f8f1bf31b28b01df4a62fb0840b5a347b
Trisha Bharti <trishabharti444@gmail.com>
2026-04-20 21:49:29 +0530

    Initial Commit

---
+++ FaceFusion/FaceFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFusionApp.swift
+//  FaceFusion
+//
+//  Created by Vikram Kumar on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FaceFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
